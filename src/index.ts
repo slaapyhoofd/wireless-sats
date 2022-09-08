8bc73672131556c43b35d74d63319bf8db75c2ae
slaapyhoofd <slaapyhoofd@protonmail.com>
2022-09-09 00:51:57 +0200

    added QR scan

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,76 @@
 import { handleLNURL, NFCReader } from 'lnurl-nfc';
+import QrScanner from 'qr-scanner';
 
-function log(text: string) {
-  const textarea = document.getElementById('textarea') as HTMLTextAreaElement;
+function log(text: string, error?: boolean) {
+  const logElement = document.getElementById('log');
 
-  if (textarea) {
-    textarea.value += `${text}\n`;
+  if (logElement) {
+    const line = document.createElement('div');
+    line.innerHTML = `${text}\n`;
+    if (error) {
+      line.style.color = 'red';
+    }
+    logElement.appendChild(line);
   }
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
   const invoice = document.getElementById('invoice') as HTMLInputElement;
-  const button = document.getElementById('button');
+  const buttonPaste = document.getElementById('paste');
+  const buttonListen = document.getElementById('listen');
+  const buttonScan = document.getElementById('scan');
+  const video = document.getElementById('video') as HTMLVideoElement;
 
   log('ready');
 
-  button?.addEventListener('click', async () => {
+  buttonPaste?.addEventListener('click', function () {
+    navigator.clipboard.readText().then(
+      (text) => {
+        if (invoice) {
+          invoice.value = text;
+        }
+        log('lnurl copied');
+      },
+      () => log('error copying lnurl', true),
+    );
+  });
+
+  const qrScanner = new QrScanner(
+    video,
+    (result) => {
+      log('QR found');
+      invoice.value = result.data;
+      qrScanner.stop();
+      if (buttonScan) {
+        buttonScan.ariaBusy = 'false';
+      }
+    },
+    { returnDetailedScanResult: true },
+  );
+
+  buttonScan?.addEventListener('click', () => {
+    qrScanner.start();
+    buttonScan.ariaBusy = 'true';
+  });
+
+  buttonListen?.addEventListener('click', async () => {
     try {
+      buttonListen.ariaBusy = 'true';
       const nfcReader = new NFCReader();
       await nfcReader.listen((lnurl) => {
-        log(`lnurl: ${lnurl}`);
-        handleLNURL(lnurl, invoice?.value, 'proxy.php').then(() => log('done!!'));
+        log(`lnurl found`);
+        handleLNURL(lnurl, invoice?.value, 'proxy.php').then((result) => {
+          if (result.success) {
+            log('invoice paid!');
+          } else {
+            log(result.message, true);
+          }
+          buttonListen.ariaBusy = 'false';
+        });
       });
       log('listening');
     } catch (e: any) {
-      log(e.message);
+      log(e.message, true);
     }
   });
 });
